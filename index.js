cad3a7e2509b25dfb8acfb0f4f0b7809f83a0a24
binitaepa <binitaepa@gmail.com>
2023-11-26 03:41:45 +0600

    property added

--- index.js
+++ index.js
@@ -30,8 +30,22 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
+
+    const propertyCollection = client.db("houseDB").collection("properties");
+
+
+    app.get('/properties', async(req, res) =>{
+        const result = await propertyCollection.find().toArray();
+        res.send(result);
+    })
+
+
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
+
+
+
+
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
