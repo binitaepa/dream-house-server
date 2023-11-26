8a8833e55b5073f16e902131448c500c0f3c3144
binitaepa <binitaepa@gmail.com>
2023-11-26 08:47:37 +0600

    propertiesid added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 // dream-house
@@ -39,6 +39,12 @@ async function run() {
         const result = await propertyCollection.find().toArray();
         res.send(result);
     })
+    app.get('/properties/:id',async(req,res)=>{
+        const id =req.params.id;
+        const query ={_id:new ObjectId(id)}
+        const result=await propertyCollection.findOne(query)
+        res.send(result)
+      })
     app.get('/reviews', async(req, res) =>{
         const result = await reviewCollection.find().toArray();
         res.send(result);
