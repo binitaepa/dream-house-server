028a469e3aba06e30c8348acdca27fe348942035
binitaepa <binitaepa@gmail.com>
2023-11-28 09:47:59 +0600

    user to  delete  added

--- index.js
+++ index.js
@@ -74,6 +74,13 @@ async function run() {
         const result = await userCollection.insertOne(user);
         res.send(result);
       });
+      app.delete('/users/:id',async(req,res)=>{
+        const id =req.params.id;
+        const query ={_id:new ObjectId(id)}
+        const result=await userCollection.deleteOne(query);
+        res.send(result)
+      })
+
       app.get('/users', async(req, res) =>{
         const result = await userCollection.find().toArray();
         res.send(result);
